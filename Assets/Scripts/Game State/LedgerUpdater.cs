ff20836448edd9093cd9064d490453b45bad1abc
Arash Tadjiki <atadjiki@gmail.com>
2019-09-07 15:45:50 -0500

    removed some trash

---
+++ Assets/Scripts/Game State/LedgerUpdater.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LedgerUpdater : MonoBehaviour
+{
+
+    public Image NeighborhoodImage;
+    public Image CopsImage;
+    public Image RivalsImage;
+
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        
+    }
+}
