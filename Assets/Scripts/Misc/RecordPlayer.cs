9eb94b100369ce59fdb83f1199f98cd4a503cc87
jdavies4741 <jdavies4741@gmail.com>
2019-09-07 21:43:26 -0600

    recordplayer and man

---
+++ Assets/Scripts/Misc/RecordPlayer.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RecordPlayer : MonoBehaviour
+{
+    public GameObject record;
+    public GameObject twistyboi;
+    public GameObject needle;
+    private Transform recordT;
+    private Transform tBT;
+    private SkinnedMeshRenderer phonograph;
+    private float bs;
+    private float p;
+    // Start is called before the first frame update
+    void Start()
+    {
+        recordT = record.transform;
+        tBT = twistyboi.transform;
+        phonograph = needle.GetComponent<SkinnedMeshRenderer>();
+        p = 1; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        recordT.Rotate(0, 3, 0);
+        tBT.Rotate(0, 0, 1);
+        bs = bs + (1f * p);
+        blendshapedelta();
+        phonograph.SetBlendShapeWeight(0, bs);
+        Debug.Log(bs);
+
+
+    }
+
+
+    void blendshapedelta()
+    {
+       
+       
+
+        if (bs < 0f)
+        {
+            p = 1;
+        }else if(bs > 100f)
+        {
+            p = -1;
+        }
+    }
+}
