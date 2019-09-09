a4cb30917cd0469beb6b545cf843d9d309e92dc5
Sumi <sumi.n.lcy@gmail.com>
2019-09-08 23:46:09 -0600

    Auto stash before merge of "master" and "origin/master"

---
+++ Assets/Scripts/Misc/CigaretteInteraction.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CigaretteInteraction : MonoBehaviour
+{
+    public float intensity;
+
+    private GameObject smokeLightObj;
+    private Light smokeLight;
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        smokeLightObj = transform.GetChild(0).gameObject;
+        smokeLight = smokeLightObj.GetComponent<Light>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        smokeLight.intensity = intensity;
+    }
+
+    public void PlayAnimation()
+    {
+        anim.Play("Smoke");
+    }
+}
